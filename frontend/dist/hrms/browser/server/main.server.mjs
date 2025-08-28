ae2212bf2232c0b6e1509bc73742c6b88db9141e
Ubuntu <ubuntu@ip-172-31-22-253.ap-south-1.compute.internal>
2025-08-28 09:10:50 +0000

    Initial commit with backend, frontend, Dockerfiles, docker-compose, Jenkinsfile

---
+++ frontend/dist/hrms/browser/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{e as a}from"./chunk-UB7CFFKM.mjs";import"./chunk-RJPWRLKX.mjs";import"./chunk-LRR3PCWL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
